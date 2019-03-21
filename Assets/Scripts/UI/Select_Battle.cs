3ed4a1f2fe269c6939db1429527daa175a5017d7
994235762@qq.com <994235762@qq.com>
2019-03-21 18:12:59 +0800

    Also,UI.Conversation&Battle

---
+++ Assets/Scripts/UI/Select_Battle.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Select_Battle : Select
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        selection();
+    }
+}
