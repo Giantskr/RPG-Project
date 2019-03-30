e437db6589936d274b0a929924c4cb540f42f562
Kappa9 <893387721@qq.com>
2019-03-30 18:29:32 +0800

    Camera optimization.

---
+++ Assets/Scripts/UI/Fading.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fading : MonoBehaviour
+{
+    void SetFading()
+	{
+		GameManager.fading = false;
+	}
+}
