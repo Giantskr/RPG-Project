bac4da1c33b84732088d6062ea9c180cb2e29c07
994235762@qq.com <994235762@qq.com>
2019-05-12 01:25:25 +0800

    Match for the change of json. 使修改后的json能用

--- Assets/Scripts/System/SkillModel.cs
+++ Assets/Scripts/System/SkillModel.cs
@@ -34,7 +34,78 @@ public class ObjectData
         public byte id;
         public string objectName;
         public string spritePath;
-
+        public string introduction;
+        public int price;
+        public string hpFormula;
+        public string mpFormula;
     }
     public ObjectInfo[] Objects;
+}
+[System.Serializable]
+public class WeaponsData
+{
+    [System.Serializable]
+    public class WeaponsInfo
+    {
+        public byte id;
+        public string objectName;
+        public string spritePath;
+        public string introduction;
+        public int level;
+        public int price;
+        public int ATK;
+        public int DEF;
+        public int MAT;
+        public int MDF;
+        public int AGI;
+        public int maxHP;
+        public int maxMP;
+        public int plusDmg;
+    }
+    public WeaponsInfo[] Weapons;
+}
+[System.Serializable]
+public class ArmorsData
+{
+    [System.Serializable]
+    public class ArmorsInfo
+    {
+        public byte id;
+        public string objectName;
+        public string spritePath;
+        public string introduction;
+        public int price;
+        public int ATK;
+        public int DEF;
+        public int MAT;
+        public int MDF;
+        public int AGI;
+        public int maxHP;
+        public int maxMP;
+        public int resistance;
+
+    }
+    public ArmorsInfo[] Armors;
+}
+public class HelmetsData
+{
+    [System.Serializable]
+    public class HelmetsInfo
+    {
+        public byte id;
+        public string objectName;
+        public string spritePath;
+        public string introduction;
+        public int price;
+        public int ATK;
+        public int DEF;
+        public int MAT;
+        public int MDF;
+        public int AGI;
+        public int maxHP;
+        public int maxMP;
+        public int resistance;
+
+    }
+    public HelmetsInfo[] Helmets;
 }
